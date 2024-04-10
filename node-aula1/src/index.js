2e8ccece7fb1a259fe1fabda841909f2ba90c72e
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-04-10 09:22:39 -0300

    Adicionando nodemon

--- node-aula1/src/index.js
+++ node-aula1/src/index.js
@@ -4,10 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! Formandos 2025');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
