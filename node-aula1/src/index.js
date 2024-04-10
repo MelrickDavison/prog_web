522ef5a553820a8fb2fc40285a1f085ae68dbb0b
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-04-10 10:29:47 -0300

    Adicionando comentarios e requisição post

--- node-aula1/src/index.js
+++ node-aula1/src/index.js
@@ -1,15 +1,19 @@
 
 const express = require('express');
-const app = express();
-const port = 3000;
+const app = express(); 
+const port = 3000; // Porta que o servidor vai rodar
+app.use(express.json()) //Preparação pro express ler json
 
 app.get('/', (req, res) => {
   res.send('Hello World! Formandos 2025');
 });
 
 app.post('/', function(req, res){
+  console.log(req.body?.nome) // ? -> Preparar que em caso de nulo não quebrar a aplicar
+  console.log(req.body?.email)
   res.send('Got a POST request');
 });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`); 
 });
