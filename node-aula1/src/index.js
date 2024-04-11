6a5c475ec4ce6cc47f5655fa07d519cb75dcf239
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-04-11 11:25:16 -0300

    Adicionando o objeto "chapa"

--- node-aula1/src/index.js
+++ node-aula1/src/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express(); 
 const port = 3000; // Porta que o servidor vai rodar
@@ -8,12 +7,23 @@ app.get('/', (req, res) => {
   res.send('Hello World! Formandos 2025');
 });
 
-app.post('/', function(req, res){
-  console.log(req.body?.nome) // ? -> Preparar que em caso de nulo não quebrar a aplicar
-  console.log(req.body?.email)
-  res.send('Got a POST request');
+app.post('/', function(req, res){ //req => Recupera o que enviou (Requisição) - res => Envia para o cliente
+ // console.log(req.body?.nome) // ? -> Preparar que em caso de nulo não quebrar a aplicar
+  let chapa = req.body.chapa
+  res.send(`Olá chapa ${chapa.nome}`);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`); 
 });
+
+/* No Postman (body): 
+{
+        "chapa": {
+            "nome": "TURN",
+            "presidencia": "Alicia",
+            "vice-presidencia": "Gabriel",
+            "secretario_geral": "Helysson"
+        }
+}
+*/
