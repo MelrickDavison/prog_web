e3f76ecfd4b5aa274d9d658d55b6a866f95a22d1
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-04-11 11:58:57 -0300

    adicionando /salvar

--- node-aula1/src/index.js
+++ node-aula1/src/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express(); 
+
 const port = 3000; // Porta que o servidor vai rodar
-app.use(express.json()) //Preparação pro express ler json
+
+app.use(express.json()) //Preparação pro express ler json - via json
+app.use(express.urlencoded) //Preparação pro express ler urlencoded - via formulário
+
+/
 
 app.get('/', (req, res) => {
   res.send('Hello World! Formandos 2025');
 });
 
+/*
 app.post('/', function(req, res){ //req => Recupera o que enviou (Requisição) - res => Envia para o cliente
  // console.log(req.body?.nome) // ? -> Preparar que em caso de nulo não quebrar a aplicar
   let chapa = req.body.chapa
   res.send(`Olá chapa ${chapa.nome}`);
+});
+*/
+
+//Rota
+app.post('/salvar', (req, res) => {
+  let nome = req.body.nome
+  res.send(`Olá ${nome}`);
 });
 
 app.listen(port, () => {
