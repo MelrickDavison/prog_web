4b079198522483057d2ff9b0d202468abc36f5e0
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-07-18 11:48:33 -0300

    Código post

--- revisao-final/index.js
+++ revisao-final/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json()) //Preparação pro express ler json - via json
+//app.use(express.urlencoded()) //Preparação pro express ler urlencoded - via formulário
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post('/salvar', (req, res) => {
+let nome = req.body.nome;
+let nota1 = req.body.nota1;
+let nota2 = req.body.nota2;
+let media = (nota1 + nota2) / 2;
+
+if (media < 6) {
+  res.status(500).json({
+    mensagem: "Estudar MAIS"
+  });
+}
 })
 
 app.listen(port, () => {
